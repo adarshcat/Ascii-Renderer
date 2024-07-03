40543b2046b1c9620c7d72d2e5563a6a4e478675
Adarsh Bharti <adarshbharti19082005@gmail.com>
2024-07-03 17:11:37 +0530

    Ascii renderer! (first)

---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// some macro which makes clear screen cross-platform (just call clrscr() to clear screen now)
+#ifdef _WIN32
+#define clrscr() system("cls")
+#else
+#include <stdio.h>
+#define clrscr() system("clear")
+#endif
+
+#define FRAME_RATE 15
+
+
+void delay(clock_t *lastTickClock){
+    clock_t currentClock = clock();
+    clock_t deltaClock = currentClock - *lastTickClock;
+
+    while (((float) deltaClock) / CLOCKS_PER_SEC < (1.0 / FRAME_RATE)){
+        currentClock = clock();
+        deltaClock = currentClock - *lastTickClock;
+    }
+
+    *lastTickClock = currentClock;
+}
+
+int main(){
+    clock_t lastTickClock = clock();
+
+    while (1){
+        clrscr();
+        printf("Hello World\n");
+
+        delay(&lastTickClock);
+    }
+
+    return 0;
+}
